18f6635fdda1c2a785d0f646f46d979c89a588f4
Gabriel de Santana <gabrielbsantana61@gmail.com>
2026-08-19 15:47:55 -0300

    feat: add storefront routes

---
+++ store/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.home,name='home'),
+    path('cardapio/',views.catalog,name='catalog'),
+    path('cadastro/',views.register,name='register'),
+    path('minha-conta/',views.account,name='account'),
+    path('pedidos/criar/',views.create_order,name='create_order'),
+    path('pedidos/<uuid:public_id>/',views.order_detail,name='order_detail'),
+    path('pagamentos/mercado-pago/webhook/',views.mercado_pago_webhook,name='mp_webhook'),
+]
