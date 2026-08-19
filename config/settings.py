81b23595619000908113a610f3e672d9c4870fc2
Gabriel de Santana <gabrielbsantana61@gmail.com>
2026-08-19 15:54:39 -0300

    security: harden sessions and enable admin otp

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,8 @@ CSRF_TRUSTED_ORIGINS = [u for u in os.environ.get('CSRF_TRUSTED_ORIGINS', '').sp
 INSTALLED_APPS = [
     'daphne', 'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
     'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles',
-    'channels', 'store',
+    'django_otp', 'django_otp.plugins.otp_totp', 'django_otp.plugins.otp_static',
+    'channels', 'store.apps.StoreConfig',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -20,6 +21,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django_otp.middleware.OTPMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -40,8 +42,10 @@ STATIC_URL='static/'; STATIC_ROOT=BASE_DIR/'staticfiles'; STATICFILES_DIRS=[BASE
 MEDIA_URL='/media/'; MEDIA_ROOT=BASE_DIR/'media'
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 LOGIN_URL='login'; LOGIN_REDIRECT_URL='account'; LOGOUT_REDIRECT_URL='home'
-SESSION_COOKIE_HTTPONLY=True; SESSION_COOKIE_SAMESITE='Lax'; CSRF_COOKIE_SAMESITE='Lax'
+SESSION_COOKIE_HTTPONLY=True; SESSION_COOKIE_SAMESITE='Lax'; SESSION_COOKIE_AGE=43200; SESSION_SAVE_EVERY_REQUEST=True
+CSRF_COOKIE_SAMESITE='Lax'
 SECURE_CONTENT_TYPE_NOSNIFF=True; X_FRAME_OPTIONS='DENY'; SECURE_REFERRER_POLICY='strict-origin-when-cross-origin'
+OTP_ADMIN_HIDE_SENSITIVE_DATA=True; OTP_TOTP_ISSUER='Nossas Delicias Admin'; OTP_TOTP_THROTTLE_FACTOR=1
 if not DEBUG:
     SECURE_SSL_REDIRECT=True; SESSION_COOKIE_SECURE=True; CSRF_COOKIE_SECURE=True
     SECURE_HSTS_SECONDS=31536000; SECURE_HSTS_INCLUDE_SUBDOMAINS=True; SECURE_HSTS_PRELOAD=True
@@ -51,3 +55,5 @@ REDIS_URL=os.environ.get('REDIS_URL')
 CHANNEL_LAYERS = {'default': {'BACKEND':'channels_redis.core.RedisChannelLayer','CONFIG':{'hosts':[REDIS_URL]}}} if REDIS_URL else {'default': {'BACKEND':'channels.layers.InMemoryChannelLayer'}}
 MERCADO_PAGO_ACCESS_TOKEN=os.environ.get('MERCADO_PAGO_ACCESS_TOKEN','')
 MERCADO_PAGO_WEBHOOK_SECRET=os.environ.get('MERCADO_PAGO_WEBHOOK_SECRET','')
+EMAIL_BACKEND=os.environ.get('EMAIL_BACKEND','django.core.mail.backends.console.EmailBackend')
+DEFAULT_FROM_EMAIL=os.environ.get('DEFAULT_FROM_EMAIL','Nossas Delícias <no-reply@example.com>')
