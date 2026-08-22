69fac1731b37db880c6f72a33a3236a554d10681
Gabriel de Santana <gabrielbsantana61@gmail.com>
2026-08-22 16:53:52 -0300

    feat: expose cafe note editing and finance reporting routes

--- store/urls.py
+++ store/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import views_finance
 
 urlpatterns=[
     path('',views.home,name='home'),
@@ -14,10 +15,14 @@ urlpatterns=[
     path('entrega/disponibilidade/',views.delivery_availability,name='delivery_availability'),
     path('cafeterias/',views.cafe_portal,name='cafe_portal'),
     path('cafeterias/cadastro/',views.cafe_apply,name='cafe_apply'),
+    path('cafeterias/pedidos/<uuid:public_id>/editar/',views_finance.cafe_order_edit,name='cafe_order_edit'),
+    path('cafeterias/pedidos/<uuid:public_id>/nota/',views_finance.cafe_note,name='cafe_note'),
     path('eventos/',views.event_portal,name='event_portal'),
     path('eventos/orcamento/',views.event_quote_create,name='event_quote_create'),
     path('pedidos/<uuid:public_id>/',views.order_detail,name='order_detail'),
     path('pedidos/<uuid:public_id>/pagar/',views.start_payment,name='start_payment'),
+    path('financeiro/',views_finance.finance_dashboard,name='finance_dashboard'),
+    path('financeiro/exportar.csv',views_finance.finance_export_csv,name='finance_export_csv'),
     path('pagamentos/retorno/',views.payment_return,name='payment_return'),
     path('pagamentos/mercado-pago/webhook/',views.mercado_pago_webhook,name='mp_webhook'),
 ]
