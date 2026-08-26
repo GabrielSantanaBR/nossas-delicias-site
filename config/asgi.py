699c5e91577b74bb2305fe31bdcf5bf404858118
Gabriel de Santana <gabrielbsantana61@gmail.com>
2026-08-26 02:29:54 -0300

    fix: initialize Django before websocket routing

--- config/asgi.py
+++ config/asgi.py
@@ -1,9 +1,18 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','config.settings')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+
+# Initialize Django before importing websocket routing. store.routing imports
+# consumers/models, which require the app registry to be ready.
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from store.routing import websocket_urlpatterns
 
-django_asgi_app = get_asgi_application()
-application = ProtocolTypeRouter({'http': django_asgi_app, 'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns))})
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+})
