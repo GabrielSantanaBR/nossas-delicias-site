78ad48ec848101cb13c09b2d26721c50a69bf761
Gabriel de Santana <gabrielbsantana61@gmail.com>
2026-09-03 19:42:57 -0300

    feat: renovar vitrine e acesso do cliente
    Redesenha a vitrine pública em um layout editorial mais convencional e melhora o login/cadastro sem reduzir as proteções existentes.

--- store/auth_views.py
+++ store/auth_views.py
@@ -2,9 +2,11 @@ from django.contrib.auth.views import LoginView, PasswordResetView
 from django.utils.decorators import method_decorator
 from django_ratelimit.decorators import ratelimit
 
+from .forms import CustomerAuthenticationForm
+
 @method_decorator(ratelimit(key='ip',rate='8/m',method='POST',block=True),name='dispatch')
 class RateLimitedLoginView(LoginView):
-    pass
+    authentication_form = CustomerAuthenticationForm
 
 @method_decorator(ratelimit(key='ip',rate='4/h',method='POST',block=True),name='dispatch')
 class RateLimitedPasswordResetView(PasswordResetView):
